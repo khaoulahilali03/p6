5a326b6d6a6f53229639c4a7198fc7db7cc1d9a0
Khaoula <hilali.khaoula@gmail.com>
2023-02-22 03:35:06 +0100

    create registration view and registration.html

---
+++ paymybuddy/src/main/java/com/opc/paymybuddy/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.opc.paymybuddy.controller;
+
+import com.opc.paymybuddy.model.User;
+import com.opc.paymybuddy.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
+    @GetMapping("/registration")
+    public String showRegistrationForm(Model model){
+        User user = new User();
+        model.addAttribute("user",user);
+        return "registration";
+    }
+
+  
+}
